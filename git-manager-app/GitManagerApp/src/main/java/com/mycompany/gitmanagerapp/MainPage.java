560cf5fb55529656875e4d63fa87e11883361e25
Maryan Bahnyuk <marbuga@gmail.com>
2017-12-15 17:38:51 +0200

    add label

--- git-manager-app/GitManagerApp/src/main/java/com/mycompany/gitmanagerapp/MainPage.java
+++ git-manager-app/GitManagerApp/src/main/java/com/mycompany/gitmanagerapp/MainPage.java
@@ -9,6 +9,7 @@ import java.awt.Cursor;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -20,19 +21,24 @@ public class MainPage extends JPanel {
 
     protected static final String pathButtonString = "Open";
     protected static final String pathInputString = "Path will be here";
+    protected static final String pathLabelString = "Repo";
     
     public MainPage() {
         setLayout(new GridLayout(0, 2));
 
+        JLabel pathLabel = new JLabel(pathLabelString);
+
         // pathInput
         JTextField pathInput = new JTextField();
         pathInput.setText(pathInputString);
+        pathLabel.setLabelFor(pathInput);
 
         // pathButton
         JButton pathButton = new JButton();
         pathButton.setText(pathButtonString);
         pathButton.setCursor(Cursor.getDefaultCursor());        
         
+        this.add(pathLabel);
         this.add(pathInput);
         this.add(pathButton);
     }
